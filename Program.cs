2f8efe70a1356018a2ad3ea4001dccfe7602f5e4
jmariotto93 <josemarioto@gmail.com>
2022-10-16 13:02:10 -0300

    Primeiro commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿//Criando a Fila
+Queue<int> fila = new Queue<int>();
+
+//Atribuindo valores a fila
+fila.Enqueue(2);
+fila.Enqueue(4);
+fila.Enqueue(6);
+fila.Enqueue(8);
+
+foreach (int item in fila)
+{
+    Console.WriteLine(item);
+}
+//Removendo o primeira da fila usando Dequeue
+Console.WriteLine($" Removendo o elemento: { fila.Dequeue()}");
+
+// atribuindo um valor ele irá para o final da fila
+fila.Enqueue(10);
+
+foreach (int item in fila)
+{
+    Console.WriteLine(item);
+}
